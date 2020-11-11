2ccdc691c358af9e1e71813125ddf917ad2899c8
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-11 22:23:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,11 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board, index)
 #  board[index] == "X" || board[index] == "O"
- board[index] != " "
+# board[index] != " "
+ if board[index] != " "
+   if board[index] != ""
+     if board[index] != nil
+    end
+  end
+end
 end
